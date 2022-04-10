3b4c4c49e854118982f76815fb37d2a231b30972
Carlos <Carlos@DESKTOP-LONCR8V>
2022-04-10 20:12:14 -0300

    Creacion de nueva clase 'Prestamo

---
+++ ProjectDemo/src/main/java/ar/edu/unju/fi/aplicaciones/model/Prestamo.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.aplicaciones.model;
+
+import java.time.LocalDate;
+
+public class Prestamo {
+	private int codigo_prestamo;
+    private int cod_libro_prestamo;
+    private int cod_autor_prestamo;
+    private LocalDate fechaout;
+    private LocalDate fechain;
+	
+    
+    public Prestamo() {
+		super();
+	}
+    
+
+}
