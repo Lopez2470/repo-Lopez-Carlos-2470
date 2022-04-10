3fabe43c11dc4a4658cb13a08d0a7a2f72306338
Carlos <Carlos@DESKTOP-LONCR8V>
2022-04-10 19:47:24 -0300

    Creacion de nueva clase 'Autor'.

---
+++ ProjectDemo/src/main/java/ar/edu/unju/fi/aplicaciones/model/Autor.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.aplicaciones.model;
+
+public class Autor {
+    private int id_autor;
+    private String nombre_autor;
+    private String apellido_autor;
+    
+    
+    
+	public Autor() {
+		super();
+	}
+
+    
+    
+}
