7f60b7ac18695b93bb163d2b2c8434931f8d3d89
Carlos <Carlos@DESKTOP-LONCR8V>
2022-04-10 19:39:13 -0300

    Creacion del package 'model' y clase 'Libro'

---
+++ ProjectDemo/src/main/java/ar/edu/unju/fi/aplicaciones/model/Libro.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.aplicaciones.model;
+
+public class Libro {
+    private String idLibro;
+    private String nomLibro;
+    private String editorial;
+    private String nomAutor;
+    private int cantidad_disponible;
+    
+	public Libro() {
+		super();
+	}
+
+}
