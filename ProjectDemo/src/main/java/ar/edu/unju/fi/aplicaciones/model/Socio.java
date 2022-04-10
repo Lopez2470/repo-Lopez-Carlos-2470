03bd492338494eb1570799a471725b470bfc1641
Carlos <Carlos@DESKTOP-LONCR8V>
2022-04-10 20:11:21 -0300

    Merge branch 'master' of https://github.com/Lopez2470/repo-Lopez-Carlos-2470.git

---
+++ ProjectDemo/src/main/java/ar/edu/unju/fi/aplicaciones/model/Socio.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.aplicaciones.model;
+
+public class Socio {
+	private int id_Socio;
+    private String nombre_Socio;
+    private String apellido_Socio;
+    
+    
+	public Socio() {
+		super();
+	}
+}
